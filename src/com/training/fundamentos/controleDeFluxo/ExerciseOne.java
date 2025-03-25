46f313b5b80ee35ae02a079e35a1495fbd663a57
Johnatan Vasconcelos <johnatanvasconcelosc@gmail.com>
2025-03-25 16:12:19 -0300

    first commit

---
+++ src/com/training/fundamentos/controleDeFluxo/ExerciseOne.java
@@ -0,0 +1,22 @@
+//Verificação de Par ou Ímpar
+//
+//Peça um número ao usuário e verifique se é par ou ímpar.
+
+package com.training.fundamentos.controleDeFluxo;
+
+import java.util.Scanner;
+
+public class ExerciseOne {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite um número para verificar se é par ou ímpar: ");
+        int number = scanner.nextInt();
+
+        if (number % 2 == 0){
+            System.out.println("O número é par");
+        }else {
+            System.out.println("O número é ímpar");
+        }
+    }
+}
---
+++ src/com/training/fundamentos/controleDeFluxo/ExerciseTwo.java
@@ -0,0 +1,29 @@
+//Maior entre dois números
+//Solicite dois números e exiba qual é o maior ou se são iguais.
+
+package com.training.fundamentos.controleDeFluxo;
+
+import java.util.Scanner;
+
+public class ExerciseTwo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("\n------------------------------------------------- \n");
+        System.out.println("Vamos comparar dois números e saber quem é o maior!");
+        System.out.println("\nDigite o primeiro número: ");
+        int numberOne = scanner.nextInt();
+        System.out.println("\nDigite o segundo número: ");
+        int numberTwo = scanner.nextInt();
+
+        System.out.println("\n------------------------------------------------- \n");
+
+        if(numberOne > numberTwo) {
+            System.out.println("O número " + numberOne + " é maior que o número " + numberTwo);
+        }else if(numberOne < numberTwo){
+            System.out.println("O número " + numberTwo + " é maior que o número " + numberOne);
+        }else {
+            System.out.println("Os números são iguais :/ " + numberOne + " = " + numberTwo);
+        }
+    }
+}
