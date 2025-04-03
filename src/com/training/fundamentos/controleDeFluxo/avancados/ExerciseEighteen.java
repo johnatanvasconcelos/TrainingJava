c02b52b4a3f5309d157692f289dd7f8452b6712e
Johnatan Vasconcelos <johnatanvasconcelosc@gmail.com>
2025-04-03 17:57:11 -0300

    adicionando mais exercícios

---
+++ src/com/training/fundamentos/controleDeFluxo/avancados/ExerciseEighteen.java
@@ -0,0 +1,14 @@
+//Sequência de Fibonacci (For ou While)
+//Peça um número N e exiba os N primeiros números da sequência de Fibonacci.
+
+package com.training.fundamentos.controleDeFluxo.avancados;
+
+import java.util.Scanner;
+
+public class ExerciseEighteen {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite um número para ver a sequência de Fibonacci");
+
+    }
+}
